76ab9f1f8feaa5ce941497dfc8e12dd6b9db99f3
Shawn Pearson <1157wcl@gmail.com>
2024-12-03 21:35:31 -0500

    switch to environment

--- src/main/java/com/dbic/catalogApi/controller/StatusController.java
+++ src/main/java/com/dbic/catalogApi/controller/StatusController.java
@@ -4,24 +4,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class StatusController {
-	@Value("${app.appVersion:notset}")
-    private String appVersion;
-	@Value("${spring.cloud.gcp.sql.instance-connection-name:notset}")
-    private String instance;
-	@Value("${spring.cloud.gcp.sql.database-name:notset}")
-    private String database;
+	// @Value("${app.appVersion:notset}")
+    // private String appVersion;
+	// @Value("${app.instance:notset}")
+    // private String instance;
+	// @Value("${app.database:notset}")
+    // private String database;
+
+	@Autowired
+    private Environment environment;
 
 	@GetMapping("/")
 		public Map<String, String> statusMessage() {
     	HashMap<String, String> map = new HashMap<>();
-    	map.put("instance", instance);
-    	map.put("Database", database);
-    	map.put("appVersion", appVersion);
-    	map.put("Status", "OK @ /api/v1/catalog");
+    	map.put("instance", environment.getProperty("instance"));
+    	map.put("Database", environment.getProperty("Database"));
+    	map.put("appVersion", environment.getProperty("appVersion"));
+    	map.put("Status", "OK");
     return map;
 	}
 }
